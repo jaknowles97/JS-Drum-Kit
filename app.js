a18d8951d8f600cb30e7702efd3dcc95f126ccd4
andrew knowles <jaknowles1997@gmail.com>
2018-05-18 17:39:22 -0400

    buttons will play the corresponding audio element

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 window.addEventListener("keydown", function (e) {
-    
+    const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) { return; }
+    audio.currentTime = 0; // if the sound is currently playing, start from the beginning if played again
+    audio.play();
 })
