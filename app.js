965d55bcf5600600de29bec31915f8310e0490a7
andrew knowles <jaknowles1997@gmail.com>
2018-05-18 22:24:00 -0400

    created grid for mobile devices and added some css styles for looks

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 window.addEventListener("keydown", function (e) {
     const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) { return; }
     audio.currentTime = 0; // if the sound is currently playing, start from the beginning if played again
     audio.play();
-})
+    key.classList.add("playing");
+
+});
+
+function removeTransition(e) {
+    if(e.propertyName !== "transform") { return; }
+
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
